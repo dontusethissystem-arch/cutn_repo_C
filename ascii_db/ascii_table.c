b76412b4b7df492c275f889494104d37f3f96da3
dontusethissystem-arch <dontusethissystem@gmail.com>
2025-09-10 06:52:51 -0400

    modified ascii_table.c

--- ascii_db/ascii_table.c
+++ ascii_db/ascii_table.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 
-#define	MAX_COLUMNS		10
+#define	MAX_COLUMNS				10
 #define	MAX_COLUMNS_WIDTH	20
 
 void print_table_header(const char* headers[MAX_COLUMNS], int ncols, int cols_width[MAX_COLUMNS]){
